08ed344a3ffbeb4ed1277cbc995f263959155f8c
Dmitry Naumov <d.naumov@adguard.com>
2022-03-11 16:35:52 +0300

    dhcp-lease: start

---
+++ collectors/dhcp-lease.php
@@ -0,0 +1,38 @@
+<?php
+// Collector name
+$_COLLECTOR['enable'] = 1;
+$_COLLECTOR['name'] = 'dhcp-lease';
+$_COLLECTOR['cmd'] = '/ip/dhcp-server/lease/print';
+if (checkCollector($_COLLECTOR['name'], $_COLLECTORS) && $_COLLECTOR['enable'] == 1) {
+	$_coll_start_time = microtime(true);
+	
+	// Making new array
+	$_ARR_COLL = $_ARR + array('collector' => $_COLLECTOR['name']);
+	
+	// Starting collecting
+	// Command to execute
+	$result = $_API -> comm($_COLLECTOR['cmd']);
+	
+	// Sending the debug-info if it's required by second arg in cli
+	if ($_DEBUG === true && $_DEBUG_COLL == $_COLLECTOR['name']) {
+		var_dump($result);
+	}
+	
+	if (empty($result)) {
+		$_OUT[] = prom('mt_collector_error', $_ARR_COLL + array('error' => 'Device had sent empty response'), 1);
+	} else {
+		// Starting the collection
+		// foreach every string as 
+		// 
+
+		}
+	}
+	
+	// Collector scrape duration
+	$_coll_end_time = microtime(true);
+	$_coll_scrape = round($_coll_end_time - $_coll_start_time, 7) * 1000;
+	
+	$_OUT[] = prom(PREFIX.'_collector_scrape_duration', $_ARR_COLL, $_coll_scrape);
+	$_OUT[] = PHP_EOL.PHP_EOL;
+	unset($_ARR_COLL);
+}
